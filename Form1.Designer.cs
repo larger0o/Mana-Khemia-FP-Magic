5ec54ab6b984ef42cf770ea8a276187104c9b7e6
larger0o <largeroliker0o@gmail.com>
2018-11-20 00:43:21 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Mana_Khemia_FP_Magic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.abrirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extraerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extraerTodoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.extraerSeleccionadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertarTodoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.insertarSeleccionadoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guardarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirToolStripMenuItem,
+            this.extraerToolStripMenuItem,
+            this.insertarToolStripMenuItem,
+            this.guardarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(297, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // abrirToolStripMenuItem
+            // 
+            this.abrirToolStripMenuItem.Name = "abrirToolStripMenuItem";
+            this.abrirToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.abrirToolStripMenuItem.Text = "Abrir";
+            this.abrirToolStripMenuItem.Click += new System.EventHandler(this.abrirToolStripMenuItem_Click);
+            // 
+            // extraerToolStripMenuItem
+            // 
+            this.extraerToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.extraerTodoToolStripMenuItem,
+            this.extraerSeleccionadoToolStripMenuItem});
+            this.extraerToolStripMenuItem.Enabled = false;
+            this.extraerToolStripMenuItem.Name = "extraerToolStripMenuItem";
+            this.extraerToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.extraerToolStripMenuItem.Text = "Extraer";
+            // 
+            // extraerTodoToolStripMenuItem
+            // 
+            this.extraerTodoToolStripMenuItem.Name = "extraerTodoToolStripMenuItem";
+            this.extraerTodoToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.extraerTodoToolStripMenuItem.Text = "Extraer todo";
+            this.extraerTodoToolStripMenuItem.Click += new System.EventHandler(this.extraerTodoToolStripMenuItem_Click);
+            // 
+            // extraerSeleccionadoToolStripMenuItem
+            // 
+            this.extraerSeleccionadoToolStripMenuItem.Name = "extraerSeleccionadoToolStripMenuItem";
+            this.extraerSeleccionadoToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.extraerSeleccionadoToolStripMenuItem.Text = "Extraer seleccionado";
+            this.extraerSeleccionadoToolStripMenuItem.Click += new System.EventHandler(this.extraerSeleccionadoToolStripMenuItem_Click);
+            // 
+            // insertarToolStripMenuItem
+            // 
+            this.insertarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.insertarTodoToolStripMenuItem,
+            this.insertarSeleccionadoToolStripMenuItem});
+            this.insertarToolStripMenuItem.Enabled = false;
+            this.insertarToolStripMenuItem.Name = "insertarToolStripMenuItem";
+            this.insertarToolStripMenuItem.Size = new System.Drawing.Size(58, 20);
+            this.insertarToolStripMenuItem.Text = "Insertar";
+            // 
+            // insertarTodoToolStripMenuItem
+            // 
+            this.insertarTodoToolStripMenuItem.Name = "insertarTodoToolStripMenuItem";
+            this.insertarTodoToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.insertarTodoToolStripMenuItem.Text = "Insertar todo";
+            this.insertarTodoToolStripMenuItem.Click += new System.EventHandler(this.insertarTodoToolStripMenuItem_Click);
+            // 
+            // insertarSeleccionadoToolStripMenuItem
+            // 
+            this.insertarSeleccionadoToolStripMenuItem.Name = "insertarSeleccionadoToolStripMenuItem";
+            this.insertarSeleccionadoToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.insertarSeleccionadoToolStripMenuItem.Text = "Insertar seleccionado";
+            this.insertarSeleccionadoToolStripMenuItem.Click += new System.EventHandler(this.insertarSeleccionadoToolStripMenuItem_Click);
+            // 
+            // guardarToolStripMenuItem
+            // 
+            this.guardarToolStripMenuItem.Enabled = false;
+            this.guardarToolStripMenuItem.Name = "guardarToolStripMenuItem";
+            this.guardarToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.guardarToolStripMenuItem.Text = "Guardar";
+            this.guardarToolStripMenuItem.Click += new System.EventHandler(this.guardarToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 495);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(297, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 17);
+            this.lblStatus.Text = "Hola.";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Archivos FP|*fp";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(260, 444);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(297, 517);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MK FP Magic";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem abrirToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ToolStripMenuItem extraerToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extraerTodoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem extraerSeleccionadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertarTodoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem insertarSeleccionadoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem guardarToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+using System.IO.Compression;
+
+namespace Mana_Khemia_FP_Magic
+{
+    public partial class Form1 : Form
+    {
+        
+        string fichero = "";
+        byte[] contenido;
+        byte[] cabecera = { 0x1f, 0x8b, 0x08, 0x00 };
+        static int size = 100;
+
+        int contador = 0;
+        //falso - fin fichero sin guardar
+        bool DetectFinFichero = true;
+
+        //aquí se guardan todas las cabeceras
+        byte[,] cabecerafichero = new byte[size,4];
+
+        //aquí se guardan los punteros de los ficheros
+        //la cabecera está 4 posiciones antes
+        ArrayList posfichero = new ArrayList();
+
+        //posicion de final de fichero
+        //NO incluido en éste
+        ArrayList finfichero = new ArrayList();
+
+        byte[,] Datos = new byte[size,250000];
+        int[] LongitudDatos = new int[size];
+
+        int longitud = 0;
+        int inicio = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void abrirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            fichero = openFileDialog1.FileName;
+            if (fichero != null)
+                LoadFile(fichero);
+        }
+
+        public void LoadFile (string fichero)
+        {
+
+            //buscar la cabecera de GZ (1F8B0800)
+            if (CargarInfo(fichero))
+            {
+                lblStatus.Text = openFileDialog1.SafeFileName + " OK";
+                GetInfo(fichero);
+                MostrarInfo();
+                extraerToolStripMenuItem.Enabled = true;
+                insertarToolStripMenuItem.Enabled = true;
+            }
+            else
+                MessageBox.Show("Archivo FP inválido");
+            
+        }
+
+
+
+
+        public void GetInfo (string fichero)
+        {
+            try
+            {
+                //copiar info
+                for (int i = 0; i < contador; i++)
+                {
+                    longitud = (int)finfichero[i] - (int)posfichero[i];
+                    LongitudDatos[i * 2] = longitud;
+                    inicio = (int)posfichero[i];
+                    Datos = Copiar(contenido, Datos, i*2, inicio, longitud);
+                }
+                //copiar ceros
+                for (int i = 0; i < contador; i++)
+                {
+                    longitud = (int)posfichero[i+1] - (int)finfichero[i];
+                    LongitudDatos[(i * 2)+1] = longitud;
+                    inicio = (int)finfichero[i];
+                    Datos = Copiar(contenido, Datos, (i*2)+1, inicio, longitud);
+                }
+            }
+            catch (Exception e)
+            { lblStatus.Text = e.ToString(); }
+
+        }
+
+        private bool CargarInfo(string fichero)
+        {
+
+            try
+            {
+                contenido = File.ReadAllBytes(fichero);
+                //tenemos el contenido en un array de bytes
+
+                contador = 0;
+                for (int i = 0; i < contenido.Length; i++)
+                {
+                    //en este caso, coincide la cabecera, se anota la posición de inicio y a leer
+                    if ((contenido[i] == cabecera[i - i]) && (contenido[i + 1] == cabecera[i - i + 1]) && (contenido[i + 2] == cabecera[i - i + 2]) && (contenido[i + 3] == cabecera[i - i + 3]))
+                    {
+                        posfichero.Add(i);
+                        DetectFinFichero = false;
+                        //guardar cabecera fichero
+                        for (int x = 0; x < 4; x++)
+                        {
+                            cabecerafichero[contador, x] = contenido[i+x-4];
+                        }
+                    }
+                    if (!DetectFinFichero) {
+                        lblStatus.Text = i.ToString();
+                        if ((contenido[i] == cabecerafichero[contador, 0]) && (contenido[i + 1] == cabecerafichero[contador, 1]) && (contenido[i + 2] == cabecerafichero[contador, 2]) && (contenido[i + 3] == cabecerafichero[contador, 3]))
+                        {
+                            finfichero.Add(i+4);
+                            contador++;
+                            DetectFinFichero = true;
+                        }
+                }
+
+                }
+                posfichero.Add(contenido.Length);
+                if (contador == 0) return false;
+                return true;
+            }
+            catch (Exception e) {
+                lblStatus.Text += e.ToString();
+                return false;
+            }
+       }
+
+        public void MostrarInfo()
+        {
+            listBox1.Items.Clear();
+            for (int i = 0; i < contador; i++)
+              listBox1.Items.Add(openFileDialog1.SafeFileName + " " + i);
+
+
+        }
+       public void RellenarCabecera()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    cabecerafichero[i, j] = 0xFF;
+                }
+            }
+        }
+
+        public static string HexToString(string HexValue)
+        {
+            string StrValue = "";
+            while (HexValue.Length > 0)
+            {
+                StrValue += System.Convert.ToChar(System.Convert.ToUInt32(HexValue.Substring(0, 2), 16)).ToString();
+                HexValue = HexValue.Substring(2, HexValue.Length - 2);
+            }
+            return StrValue;
+        }
+
+        public byte[,] Copiar (byte[] origen, byte[,] destino, int indice, int inicio, int longitud)
+        {
+            try
+            {
+                for (int i = 0; i < longitud; i++)
+                    destino[indice, i] = origen[inicio + i];
+            }
+            catch
+            {
+                for (int i = origen.Length; i < longitud; i++)
+                    destino[indice, i] = 0x00;
+            }
+            return destino;
+        }
+
+        public byte[] Leer(byte[,] origen, int indice, int longitud)
+        {
+            byte[] destino = new byte[250000];
+            for (int i = 0; i < longitud; i++)
+                destino[i] = origen[indice,i];
+            return destino;
+        }
+
+        public void extraerTodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            insertarToolStripMenuItem.Enabled = true;
+            FileStream descomprimirFS;
+            GZipStream descomprimir;
+            string filename = openFileDialog1.SafeFileName.Substring(0, (openFileDialog1.SafeFileName.Length - 3));
+            DirectoryInfo dir = new DirectoryInfo(filename);
+            if (!dir.Exists)
+                dir.Create();
+            for (int i = 0; i < contador; i++)
+            {
+                try
+                {
+                    descomprimirFS = File.Create(filename + "\\" + filename + i + ".tm2");
+                    descomprimir = new GZipStream(new MemoryStream(Leer(Datos, i * 2, LongitudDatos[i * 2])), CompressionMode.Decompress);
+                    descomprimir.CopyTo(descomprimirFS);
+                    descomprimir.Dispose();
+                    descomprimirFS.Dispose();
+                }
+                catch { }              
+            }
+            MessageBox.Show("Hecho");
+
+
+        }
+
+        private void extraerSeleccionadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            insertarToolStripMenuItem.Enabled = true;
+            FileStream descomprimirFS;
+            GZipStream descomprimir;
+            int selected = listBox1.SelectedIndex;
+            string filename = openFileDialog1.SafeFileName.Substring(0, (openFileDialog1.SafeFileName.Length - 3));
+            DirectoryInfo dir = new DirectoryInfo(filename);
+            if (!dir.Exists)
+                dir.Create();
+            if (selected != -1)
+            {
+                try
+                {
+                    descomprimirFS = File.Create(filename + "\\" + filename + selected + ".tm2");
+                    descomprimir = new GZipStream(new MemoryStream(Leer(Datos, selected * 2, LongitudDatos[selected * 2])), CompressionMode.Decompress);
+                    descomprimir.CopyTo(descomprimirFS);
+                    MessageBox.Show("Hecho");
+                    descomprimir.Dispose();
+                    descomprimirFS.Dispose();
+                }
+                catch { MessageBox.Show("¡Error"); }
+            }
+        }
+
+        private void insertarSeleccionadoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int selected = listBox1.SelectedIndex;
+                string filename = openFileDialog1.SafeFileName.Substring(0, (openFileDialog1.SafeFileName.Length - 3));
+
+
+                byte[] tmp = File.ReadAllBytes(filename + "\\" + filename + selected + ".tm2");
+                byte[] tmp2 = Compress(tmp);
+
+                Copiar(tmp2, Datos, selected * 2, 0, LongitudDatos[selected * 2]);
+                if (!listBox1.Items[selected].ToString().Contains("*"))
+                    listBox1.Items[selected] += "*";
+                MessageBox.Show("Hecho");
+                guardarToolStripMenuItem.Enabled = true;
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Falta la imagen a reinsertar.");
+
+
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error. El fichero está en uso por otro programa.");
+            }
+
+
+}
+
+        private void insertarTodoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string filename = openFileDialog1.SafeFileName.Substring(0, (openFileDialog1.SafeFileName.Length - 3));
+            byte[] tmp;
+            try
+            {
+                for (int i = 0; i < contador; i++)
+                {
+                    tmp = File.ReadAllBytes(filename + "\\" + filename + i + ".tm2");
+                    tmp = Compress(tmp);
+                    Copiar(tmp, Datos, i * 2, 0, LongitudDatos[i * 2]);
+                    if (!listBox1.Items[i].ToString().Contains("*"))
+                        listBox1.Items[i] += "*";
+                }
+                MessageBox.Show("Hecho");
+                guardarToolStripMenuItem.Enabled = true;
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Falta alguna de las imágenes a reinsertar. Asegúrate de que haya {0} archivos.", contador.ToString());
+
+
+            }
+            catch (IOException ex)
+            {
+                MessageBox.Show("Error. Alguno de los ficheros está en uso por otro programa.");
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+
+            }
+
+
+
+        }
+
+        private void guardarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string filename = openFileDialog1.SafeFileName;
+                ArrayList tmp = new ArrayList();
+                tmp.Add(cabecerafichero[0, 0]);
+                tmp.Add(cabecerafichero[0, 1]);
+                tmp.Add(cabecerafichero[0, 2]);
+                tmp.Add(cabecerafichero[0, 3]);
+
+                for (int i = 0; i < contador * 2; i++)
+                {
+                    for (int j = 0; j < LongitudDatos[i]; j++)
+                    {
+                        tmp.Add(Datos[i, j]);
+                    }
+                }
+                byte[] tmparray = (byte[])tmp.ToArray(typeof(byte));
+                File.WriteAllBytes("NEW-" + filename, tmparray);
+                MessageBox.Show("Hecho");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error. " + ex.ToString());
+
+            }
+
+        }
+
+
+        public byte[] Compress(byte[] data)
+        {
+            using (var compressedStream = new MemoryStream())
+            {
+                using (var zipStream = new GZipStream(compressedStream, CompressionMode.Compress))
+                {
+                    zipStream.Write(data, 0, data.Length);
+                    zipStream.Close();
+                    return compressedStream.ToArray();
+                }
+            }
+        }
+
+    }
+}
